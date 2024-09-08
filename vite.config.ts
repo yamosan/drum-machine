cebbf1dd07c0047a182d7be80dca98d781c072a7
yamosan <yamorimasato@gmail.com>
2024-09-09 01:34:36 +0900

    chore: pnpm add -D vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA()],
+	plugins: [react(), VitePWA(), tsconfigPaths({ root: "./" })],
 });
