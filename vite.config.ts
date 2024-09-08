fb51c92f1ea2de6aca6dff3063bd3477e5e58b59
yamosan <yamorimasato@gmail.com>
2024-09-09 02:01:30 +0900

    feat: スタイル関連のパッケージを追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA(), tsconfigPaths({ root: "./" })],
+	plugins: [
+		react(),
+		VitePWA(),
+		tsconfigPaths({ root: "./" }),
+		vanillaExtractPlugin(),
+	],
 });
