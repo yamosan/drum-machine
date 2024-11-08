374a529b81aa66d090a75294a19d8ab8b5664c84
yamosan <yamorimasato@gmail.com>
2024-11-09 03:07:50 +0900

    feat: <link rel="apple-touch-startup-image" /> に対してbase pathが適用されるようにプラグインを実装

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, type ResolvedConfig, type Plugin } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { parse } from "node-html-parser";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,6 +13,7 @@ export default defineConfig(({ mode }) => {
 		plugins: [
 			react(),
 			VitePWA({
+				base: env.BASE_PATH,
 				// /public にあるファイルのみキャッシュ可能
 				includeAssets: ["favicon.svg"],
 				manifest: {
@@ -55,7 +58,35 @@ export default defineConfig(({ mode }) => {
 					enabled: true,
 				},
 			}),
+			resolveAppleTouchStartupImageBasePathPlugin(),
 			tsconfigPaths({ root: "./" }),
 		],
 	};
 });
+
+/**
+ * vite の UserConfig.base　オプションでは、特定のタグの href や src に対してのみ base path を適用している
+ * そのため、<link rel="apple-touch-startup-image" /> に対しても base path を適用するためのプラグインを作成
+ */
+function resolveAppleTouchStartupImageBasePathPlugin(): Plugin {
+	let resolvedConfig: ResolvedConfig;
+	return {
+		name: "resolve-apple-touch-startup-image-base-path",
+		configResolved(config) {
+			resolvedConfig = config;
+		},
+		transformIndexHtml(html) {
+			const root = parse(html);
+			const targets = root.querySelectorAll(
+				'link[rel="apple-touch-startup-image"]',
+			);
+			for (const target of targets) {
+				const href = target.getAttribute("href");
+				if (href?.startsWith("/")) {
+					target.setAttribute("href", path.join(resolvedConfig.base, href));
+				}
+			}
+			return root.toString();
+		},
+	};
+}
