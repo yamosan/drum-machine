5abd99b55a490344080f25f259006380aa2ff347
yamosan <yamorimasato@gmail.com>
2024-11-09 19:39:57 +0900

    feat: pnpm add pwacompat

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 
+// クライアントサイドで動的に splash screen を生成する
+import "pwacompat";
+
 const root = document.getElementById("root");
 if (!root) {
 	throw new Error("Root element not found");
